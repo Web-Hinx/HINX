09c097dc9d8f41ad8ca8942f2d53abed4e329397
Hinx <Hinx.Solutions@hotmail.com>
2024-09-15 01:25:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const createMailOptions = (fullName, email, phone, company, message) => {
 };
 
 // POST endpoint for sending emails
-app.post('/send-email', async (req, res) => {
+app.post('/api/send-email', async (req, res) => {
   const { fullName, email, phone, company, message } = req.body;
 
   if (!fullName || !email || !phone || !message) {
